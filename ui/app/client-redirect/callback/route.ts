77ac401d51c959a4ab9c739c9aec03f1aafd7683
CopyCoder(VJ) <107760796+vanha777@users.noreply.github.com>
2024-05-04 01:24:28 +1000

    x

---
+++ ui/app/client-redirect/callback/route.ts
@@ -0,0 +1,43 @@
+import { useAuth } from "@/components/providers/authProvider";
+import { createClient } from "@/utils/supabase/server";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  // @ts-ignore
+  const { user, public_user } = useAuth();
+  console.log("this is banana ",public_user);
+  const node_crypto = require('crypto');
+  const jwt = require('jsonwebtoken');
+  // The `/auth/callback` route is required for the server-side auth flow implemented
+  // by the SSR package. It exchanges an auth code for the user's session.
+  // https://supabase.com/docs/guides/auth/server-side/nextjs
+  const requestUrl = new URL(request.url);
+  const client_id = requestUrl.searchParams.get("client_id");
+  const origin = requestUrl.origin;
+  let info = {
+    "project": {},
+    "public_users": {}
+  };
+  let token = "";
+  console.log("this is client_id ", client_id);
+  if (client_id) {
+    const supabase = createClient();
+    const { data: project, error } = await supabase.from('project').select('*').eq('client_id', client_id).limit(1);
+    if (error) {
+      console.error('Error inserting data:', error);
+      alert('Failed to create project');
+    } else {
+      const { data: user_info, error } = await supabase.from('public_users').select('*').eq('id', project[0].user_id).limit(1);
+      info.project = project;
+      info.public_users = user_info;
+      let payload = JSON.stringify(info);
+      token = jwt.sign(payload, project[0].client_secret);
+      console.log('This is JWT:', token);
+    }
+    // await supabase.auth.exchangeCodeForSession(code);
+    // console.log("this is auth from Google ",code);
+  }
+
+  // URL to redirect to after sign up process completes
+  // return NextResponse.redirect(`${origin}/client-redirect?jwt_token=${token}`);
+}
