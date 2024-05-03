d0a3cd9bf57303cdbea0110913df98c20bed21ad
CopyCoder(VJ) <107760796+vanha777@users.noreply.github.com>
2024-05-03 22:29:45 +1000

    x

--- ui/app/register/page.tsx
+++ ui/app/register/page.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 import { headers } from "next/headers";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
-import { LoginButtonGoogle } from "@/components/auth/loginWithGoogle";
+import { RegisterForm } from "@/components/register/registerForm";
 
-export default function Register(){
+export default function Register() {
 
 
   return (
     <div className="flex flex-col items-center">
-
+      <RegisterForm user_id="7be3857e-4769-4d6c-8b04-74007a8d3d6f"/>
     </div>
   );
 }
