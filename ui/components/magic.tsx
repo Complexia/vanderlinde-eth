d0f2d7be49fede424f384f63142e859a8b69fec2
Complexia <complexia701@gmail.com>
2024-05-03 22:31:12 +1000

    smart wallet created

--- ui/components/magic.tsx
+++ ui/components/magic.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { LoginButtonGoogle } from "./auth/loginWithGoogle";
 import { useAuth } from "./providers/authProvider";
 import { createClient } from "@/utils/supabase/client";
+import { useAccount, useAuthenticate, useSmartAccountClient } from "@alchemy/aa-alchemy/react";
+import { ProfileCard } from "./ProfileCard";
 
 
 
@@ -13,21 +15,26 @@ const Magic = () => {
     const signOut = async () => {
         const supabase = createClient();
         await supabase.auth.signOut();
-        
-      };
+
+    };
 
     // @ts-ignore
     const { user, public_user } = useAuth();
+    const { authenticate, isPending: isAuthenticatingUser } = useAuthenticate();
+    const { isLoadingAccount } = useAccount({
+        type: "MultiOwnerModularAccount",
+        skipCreate: true,
+      });
 
     // create AA wallet here if user is true
-    useEffect(() => {
-        if (user) {
-            console.log('create wallet')
-        }
 
-    }, [user])
+    const { client } = useSmartAccountClient({
+        type: "MultiOwnerModularAccount",
+      });
 
+      console.log(client)
 
+    
     console.log(user, public_user)
     return (
         <div>
@@ -36,7 +43,11 @@ const Magic = () => {
                 user ?
                     <div className="flex flex-col items-center justify-center space-y-4">
                         <h1>Logged in as {user.email}</h1>
-                        <button className="btn" onClick={() => signOut()}>Sign out</button>
+
+                        <ProfileCard />
+                        
+                        <button className="btn" onClick={() => authenticate({"type": "email", "email": user.email}) }>Create Smart Wallet</button>
+                        <button className="btn" >Sign out</button>
                     </div>
 
                     :
