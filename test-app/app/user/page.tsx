7187ef017b54ced146d0d00069799c77bf503d3a
CopyCoder(VJ) <107760796+vanha777@users.noreply.github.com>
2024-05-05 05:46:49 +1000

    x

--- test-app/app/user/page.tsx
+++ test-app/app/user/page.tsx
@@ -309,7 +309,7 @@ export default async function Index(context) {
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
                 <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-7xl px-3 pt-20">
                     <main className="flex-1 flex flex-col gap-6">
-                        <h1 className="mb-5 text-5xl font-bold">Shop With Us</h1>
+                        <h1 className="mb-5 text-5xl font-bold">Discover Tokenized Goods</h1>
                         <div className="divider"></div>
 
                         {response && (
--- ui/app/user/page.tsx
+++ ui/app/user/page.tsx
@@ -307,7 +307,7 @@ export default async function Index(context) {
 
             <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-7xl px-3 pt-20">
                 <main className="flex-1 flex flex-col gap-6">
-                    <h1 className="mb-5 text-5xl font-bold">Shop With Us</h1>
+                    <h1 className="mb-5 text-5xl font-bold">Discover Tokenized Goods</h1>
                     <div className="divider"></div>
 
                     {response && (
