8a3c37cdbbe1a5c8a8c78f57bcae361f67df1407
Complexia <complexia701@gmail.com>
2024-05-05 07:06:29 +1000

    fixed and changed

--- ui/components/magicFunctions/texas-button.tsx
+++ ui/components/magicFunctions/texas-button.tsx
@@ -18,6 +18,8 @@ const TexasComponent = ({ target_chain, target_address, txn_data, origin_url }) 
     const [send, setSend] = useState(null);
     const [result, setResult] = useState(null);
 
+    console.log("txnss" , target_address)
+
     // const { client } = useSmartAccountClient({
     //     type: "MultiOwnerModularAccount",
     //     gasManagerConfig: {
@@ -49,6 +51,7 @@ const TexasComponent = ({ target_chain, target_address, txn_data, origin_url }) 
 
     // @ts-ignore
     const { public_user } = useAuth();
+    const [txnResult, setTxnResult] = useState(null);
 
     let target_address_sliced = target_address.slice(0, 4) + '..' + target_address.slice(-4);
     let result_target = null;
@@ -71,6 +74,7 @@ const TexasComponent = ({ target_chain, target_address, txn_data, origin_url }) 
             setResult(data);
             result_target = data.slice(0, 4) + '..' + target_address.slice(-4);
         }
+        setTxnResult(data);
         setSend(false)
         console.log("this is return from txn", data);
     }
@@ -120,7 +124,7 @@ const TexasComponent = ({ target_chain, target_address, txn_data, origin_url }) 
                                             className="w-full transform rounded-lg bg-[#363FF9] p-3 font-semibold text-[#FBFDFF] transition duration-500 ease-in-out hover:scale-105 disabled:bg-[#C0D4FF] disabled:hover:scale-100 dark:disabled:bg-[#4252C5]"
                                             onClick={() => executeTxn()}
                                         >
-                                            Send Transaction
+                                           <IconSpinner /> 
                                         </button>
                                     }
                                     {!send && !result &&
@@ -166,8 +170,15 @@ const TexasComponent = ({ target_chain, target_address, txn_data, origin_url }) 
                                     <div className="card w-96 bg-base-100 shadow-xl image-full">
                                         <figure><img src="https://www.patriotsoftware.com/wp-content/uploads/2022/01/what-is-blockchain-1.jpg" alt="Shoes" /></figure>
                                         <div className="card-body">
-                                            <h2 className="card-title">Cross-chain?</h2>
-                                            <p>Vanderlinde make it easier than ever!</p>
+                                            {target_chain !== "Base Sepolia" && (
+                                                <div className="flex flex-col space-y-2">
+                                                <h2 className="card-title">From Base Sepolia to {target_chain}</h2>
+                                                <p>Vanderlinde make it easier than ever!</p>
+                                                
+                                                </div>
+
+                                            )}
+                                            
                                         </div>
                                     </div>
                                 }
@@ -188,12 +199,21 @@ const TexasComponent = ({ target_chain, target_address, txn_data, origin_url }) 
                                         </label>
                                         <label className="input input-bordered flex items-center gap-2">
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
-                                            <input disabled type="text" className="grow" placeholder={`${result_target}`} />
+                                            <input disabled type="text" className="grow" placeholder={`${txnResult}`} />
                                         </label>
                                         <label className="input input-bordered flex items-center gap-2">
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
                                             <input disabled type="text" className="grow" value={`${displayData}`} />
                                         </label>
+                                        <Link href={{
+                                            pathname: origin_url,
+                                            query: {
+                                                txnResult,
+                                                
+                                            },
+                                        }}>
+                                            <button className="btn btn-secondary">Return to client</button>
+                                        </Link>
                                     </div>
                                 }
                                 {/* <button onClick={handleSend}>Send Data</button> */}
